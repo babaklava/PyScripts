f3aca74266af80d7105907f87e55eafcd9792c31
babaklava <babaklava@users.noreply.github.com>
2015-04-28 11:46:57 -0400

    added disclaimer and license

--- Sping.py
+++ Sping.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""
-A VERY basic python script to rapidly ping hosts on the network using multithreading.
-"""
-
+#
+# A VERY basic python script to rapidly ping hosts on the network using multithreading.
+#     Copyright (C) 2015  babaklava
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------
+# Sping was written for education purposes only
+# Its developer/s decline all responsibility:
+#	– in case the tool is used for malicious purposes or in any illegal context;
+#	– in case the tool crashes your system or other systems.
+#
+#
 import threading
 from queue import Queue
 import sys
