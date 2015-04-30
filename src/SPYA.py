2147f5daedfa42dd3688652bde2b4e6237b44958
babaklava <babaklava@users.noreply.github.com>
2015-04-30 13:40:51 -0400

    adding more stuff and fixing folder tree

---
+++ src/SPYA.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# A basic python script to rapidly ping hosts on the network using multithreading.
+#     Copyright (C) 2015  babaklava
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------
+# Sping was written for education purposes only
+# It's developer/s decline all responsibility:
+#	– in case the tool is used for malicious purposes or in any illegal context;
+#	– in case the tool crashes your system or other systems.
+#
+#
+print ("          ____  ____ __  ___      ")
+print ("         / ___||    \\\ \/ / \     ")
+print ("         \___ \|  __/ \  / _ \    ")
+print ("          ___) | |    / / ___ \   ")
+print ("         |____/|_|   /_/_/   \_\  ")
+print ("                                  ")                   
+print ("        SIMPLE PYTHON APPLICATION ")               
+                   
