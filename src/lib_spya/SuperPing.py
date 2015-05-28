b287503bd5f4f945e5df3904193cea33fbf4e87b
babaklava <babaklava@users.noreply.github.com>
2015-05-28 12:56:16 -0400

    changed to a class

--- src/lib_spya/SuperPing.py
+++ src/lib_spya/SuperPing.py
@@ -17,7 +17,7 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # --------------------------------------------------------------------------------
-# Sping.py was written for education purposes only
+# SuperPing.py was written for education purposes only
 # It's developer/s decline all responsibility:
 #	– in case the tool is used for malicious purposes or in any illegal context;
 #	– in case the tool crashes your system or other systems.
@@ -28,63 +28,67 @@ from queue import Queue
 import sys
 import subprocess
 import ipaddress
-import time
-
-# Set a print lock
-print_lock = threading.Lock()
-
-# create a list that holds all IPs to be pinged
-network = []
-
-# get input from user
-try:
-    host = input("\nEnter IP/Mask: ")
-    network = ipaddress.IPv4Network(host)
-except ValueError:
-    print("Error the IP Address/Netmask is invalid for IPv4:", host)
-except KeyboardInterrupt:
-    sys.exit(0)
-
-# count how many IPs were pinged and how many responded
-addr_pinged = 0
-addr_resp = 0
 
 
-# the actual function to ping the host from the queue
-def sping(ip):
-    status, result = subprocess.getstatusoutput("ping -c1 -w1 " + str(ip))
-    global addr_pinged
-    addr_pinged += 1
-    if status == 0:
-        global addr_resp
-        addr_resp += 1
-        with print_lock:
-            print("Host", ip, "is up")
+class SuperPing(object):
+    def __init__(self):
+        # count how many IPs were pinged and how many responded
+        self.addr_resp = 0
+        self.addr_pinged = 0
+        # Set a print lock
+        self.print_lock = threading.Lock()
+        # setting up empty queue
+        self.q = Queue()
 
+    def main(self, netip):
+        import time
 
-# instruction for each worker
-def threader():
-    while True:
-        ip = q.get()
-        sping(ip)
-        q.task_done()
+        # create a list that holds all IPs to be pinged
+        network = []
 
-# setting up empty queue
-q = Queue()
+        # verify user input
+        try:
+            network = ipaddress.IPv4Network(netip)
+        except ValueError:
+            print("Error the IP Address/Netmask is invalid for IPv4:", netip)
+        except KeyboardInterrupt:
+            sys.exit(0)
 
-# Creating threads, by changing the number in range you will increase amount of threads
-for x in range(256):
-    t = threading.Thread(target=threader)
-    t.daemon = True
-    t.start()
+        # Creating threads (by changing the number in range you will increase amount of threads)
+        for i in range(256):
+            t = threading.Thread(target=self.threader)
+            t.daemon = True
+            t.start()
 
-start = time.time()
+        self.start = time.time()
 
-# building the queue
-for ip in network:
-    q.put(ip)
+        # building the queue
+        for ip in network:
+            self.q.put(ip)
 
-q.join()
+        self.q.join()
 
-time = str(time.time()-start)
-print("Sping done:", addr_pinged, "IP address (", addr_resp, "host up )", "entire job took -", str(time[:3]), "sec")
+        time = str(time.time() - self.start)
+        print("SuperPing finished:", self.addr_pinged, "IP address pinged (", self.addr_resp, "responded )", "entire job took -",
+              str(time[:3]), "sec")
+
+    # the actual function to ping the host from the queue
+    def sping(self, ip):
+        status, result = subprocess.getstatusoutput("ping -c1 -w1 " + str(ip))
+        self.addr_pinged += 1
+        if status == 0:
+            self.addr_resp += 1
+            with self.print_lock:
+                print("Host", ip, "responded")
+
+    # instruction for each worker
+    def threader(self):
+        while True:
+            ip = self.q.get()
+            self.sping(ip)
+            self.q.task_done()
+
+if __name__ == '__main__':
+    sping = SuperPing()
+    ip_input = input("Enter (IP Address/Mask): ")
+    sping.main(ip_input)
