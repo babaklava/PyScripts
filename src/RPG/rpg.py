394f3ed359739d9712959308d674c683267c67e7
babaklava <babaklava@users.noreply.github.com>
2015-05-29 14:03:10 -0400

    created seperate folders and cleaned up code

--- src/RPG/rpg.py
+++ src/RPG/rpg.py
@@ -22,9 +22,6 @@
 #   – in case the tool is used for malicious purposes or in any illegal context;
 #   – in case the tool crashes your system or other systems;
 #   - in case the tool causes any loss of data or personal information;
-# -------------------------------------------------------------------------------------
-# * pycrypto is not included with python and will need to be downloaded
-#   - pip install pycrypto
 #
 #
 import string
@@ -33,6 +30,7 @@ from Crypto.Random import random
 
 
 class RandomPasswordGenerator(object):
+    """ Random Password Generator (RPG) Securely generate a random password. """
     def __init__(self):
         self.ascii_lower = string.ascii_lowercase
         self.ascii_upper = string.ascii_uppercase
@@ -41,32 +39,49 @@ class RandomPasswordGenerator(object):
 
     def ranpassgen(self, passlen, lower, upper, spec, dig):
 
+        """ Constructs a random password using the provided parameters.
+
+        :param passlen: the length of the password to be generated
+        :param lower: use lower case characters in the password
+        :param upper: use upper case characters in the password
+        :param spec: use special characters in the password
+        :param dig: use digits in the password
+        :return: Returns randomly generated password
+        """
         char_set = []
 
         if lower is True:
             char_set.append(self.ascii_lower)
+
         if upper is True:
             char_set.append(self.ascii_upper)
+
         if spec is True:
             char_set.append(self.special)
+
         if dig is True:
             char_set.append(self.digits)
+
         characters = "".join(char_set[:4])
         password = "".join(random.choice(characters) for i in range(int(passlen)))
+
         return password
 
 if __name__ == '__main__':
     rpg = RandomPasswordGenerator()
+
     parser = argparse.ArgumentParser(usage="python3 rpg.py <passlen> [<args>]", add_help=False)
 
-    parser.add_argument("passlen", type=int)
+    req = parser.add_argument_group("Required")
+    req.add_argument("passlen", help="Password Length.", type=int)
 
-    group = parser.add_argument_group("options")
-    group.add_argument("-h", "--help", help="Show this Help Message", action="help")
-    group.add_argument("-l", "--lower", help="Use lower case letters", action="count")
-    group.add_argument("-u", "--upper", help="Use upper case letters", action="count")
-    group.add_argument("-s", "--special", help="Use special characters (includes '-')", action="count")
-    group.add_argument("-d", "--digits", help="Use numbers", action="count")
+    group = parser.add_argument_group("Optional")
+    group.add_argument("-h", "--help", help="Show this Help Message.", action="help")
+    group.add_argument("-l", "--lower", help="Use lower case letters.", action="count")
+    group.add_argument("-u", "--upper", help="Use upper case letters.", action="count")
+    group.add_argument("-s", "--special", help="Use special characters (includes '-').", action="count")
+    group.add_argument("-d", "--digits", help="Use numbers.", action="count")
+
     args = parser.parse_args()
 
     args_list = []
